ada18b1b4ad08f31dcd4634500c51f2579b4c7c9
Zaid <madin1014@gmail.com>
2025-10-16 20:15:03 +0700

    refactor: remove Pokemon repository and related classes
    - Deleted PokemonRepositoryImpl and its interface to streamline data handling.
    - Removed PokemonRemoteSource and its implementation to eliminate unnecessary network calls.
    - Cleaned up domain entities including PokemonDataEntity, PokemonEntity, and PokemonEvolutionEntity.
    - Removed use cases for fetching Pokemon details and lists.
    - Eliminated the PokemonBloc and associated event/state classes to simplify state management.
    - Deleted presentation components including PokemonDetailPage, PokemonPage, and related widgets.

--- feature/pokemon/domain/repository/pokemon_repository.dart
+++
@@ -1,9 +0,0 @@
-import 'package:fpdart/fpdart.dart';
-import 'package:pokebag_flutter/core/utils/error_entity.dart';
-import 'package:pokebag_flutter/feature/pokemon/domain/entity/pokemon_data_entity.dart';
-import 'package:pokebag_flutter/feature/pokemon/domain/entity/pokemon_entity.dart';
-
-abstract class PokemonRepository {
-  Future<Either<ErrorEntity, PokemonEntity>> getPokemons(String offset);
-  Future<Either<ErrorEntity, PokemonDataEntity>> getPokemon(String id);
-}
